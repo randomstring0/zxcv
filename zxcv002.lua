eceecad0e018292688a1951b5ef08c0d88ba26f9
randomstring0 <sandshowcase@gmail.com>
2025-12-17 09:38:23 +0700

    Create zxcv002

---
+++ zxcv002.lua
@@ -0,0 +1,18 @@
+command:
+
+-sit <none>            -- Sit anywhere (toggleable)
+-noclip <none>         -- Walk through walls
+-renim <none>          -- Disable animations (toggleable)
+-s6 <none>             -- Save current outfit as R6
+-s15 <none>            -- Save current outfit as R15
+-gh <assetId | list>   -- Add hat by asset ID (stackable, persists after respawn)
+                        ex: -gh 3033910400,3409612660,3438342658,3398308134
+-ch <none>             -- Clear worn hats and reset saved hats from -gh
+-re <none>             -- Instantly respawn
+-rs <none>             -- Refresh character at current position
+-gr <none>             -- Instant respawn (unstable)
+-buy <assetId | list>  -- Bulk purchase hats (stackable)
+                        ex: -buy 3033910400,3409612660,3438342658,3398308134
+-net <none>            -- Force network ownership to your character
+-r6 <none>             -- Switch character to R6
+-r15 <none>            -- Switch character to R15
